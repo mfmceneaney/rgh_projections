cf44ab5ffc7874a964f31fe9f3108fa833c5564a
Matthew McEneaney <matthew.mceneaney@duke.edu>
2025-04-24 14:49:14 -0400

    feat: Add check for when trigger value is less than nmax.

--- jobs/rgh_simulation/mc_rgh/setup.sh
+++ jobs/rgh_simulation/mc_rgh/setup.sh
@@ -2,10 +2,16 @@
 cd $RGH_PROJECTIONS_HOME/jobs/rgh_simulation/mc_rgh
 i=1
 
-export NITERATIONS=`echo "$CLASDIS_TRIG/$CLASDIS_NMAX" | bc`
+export CLASDIS_TRIG_HALF=`echo "$CLASDIS_TRIG/2" | bc`
+if (($CLASDIS_TRIG_HALF<$CLASDIS_NMAX)); then
+    export NITERATIONS=2
+else
+    export NITERATIONS=`echo "$CLASDIS_TRIG/$CLASDIS_NMAX" | bc`
+fi
 export NEVENTS=$CLASDIS_NMAX
 export NITERATIONS_HALF=`echo "$NITERATIONS/2" | bc`
 
+# Loop number of files
 while [ $i -le $NITERATIONS ]
 do
 echo "$i > $PWD/submit$i.sh"
@@ -16,7 +22,7 @@ cp job.sh job$i.sh
 cp submit.sh submit$i.sh
 
 # Replace clasdis file prefix and gemc gcard depending on if the lund file has positive or negative target polarization
-if ((!$CLASDIS_GEN_PM || $i<$NITERATIONS_HALF)); then #NOTE: ASSUME THAT POSITIVE POLARIZATIONS ARE FIRST THEN NEGATIVE.
+if ((!$CLASDIS_GEN_PM || $i<=$NITERATIONS_HALF)); then #NOTE: ASSUME THAT POSITIVE POLARIZATIONS ARE FIRST THEN NEGATIVE.
     sed -i "s;out_tp_CLASDIS_POL_;${CLASDIS_PREFIX}${CLASDIS_POL}_idx_;g" job$i.sh
 else
     sed -i "s;out_tp_CLASDIS_POL_;${CLASDIS_PREFIX}-${CLASDIS_POL}_idx_;g" job$i.sh
