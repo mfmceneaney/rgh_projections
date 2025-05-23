80da3521eb3e0e79434aa6c9c609969601aa415b
Matthew McEneaney <matthew.mceneaney@duke.edu>
2025-05-23 18:43:54 -0400

    fix: Set output directories for saga submit scripts.

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,3 +6,8 @@ for file in $RGH_PROJECTIONS_HOME/jobs/*/*/*.yaml; do
     sed -i.bak "s;/RGH_PROJECTIONS_VOL_DIR;$RGH_PROJECTIONS_VOL_DIR;g" $file
     sed -i.bak "s;/RGC_TPOL_CSV;$RGC_TPOL_CSV;g" $file
 done
+
+# Fix for now so that job output directly can be set by saga python libraries
+for file in $RGH_PROJECTIONS_HOME/jobs/saga/*/submit.sh; do
+    sed -i.bak "s;\$RGH_PROJECTIONS_HOME;$RGH_PROJECTIONS_HOME;g" $file
+done
